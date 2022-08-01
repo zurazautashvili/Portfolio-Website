9bbb72e86da97fb067fd955a5f0aea8d2471b685
Zura Zautashvili <zurazautashvili56@gmail.com>
2022-08-01 23:20:11 +0400

    Made some changes to nav bar.

--- script.js
+++ script.js
@@ -3,10 +3,12 @@
 const menu = document.querySelector(".icon-menu");
 const list = document.querySelector(".list");
 const listClose = document.querySelector(".list-close");
+const nameShrank = document.querySelector(".siteName");
 menu.addEventListener("click", function () {
   menu.classList.add("hidden");
   list.classList.remove("hidden");
   listClose.classList.remove("hidden");
+  nameShrank.classList.add("shrank");
   document.querySelectoral(".list-item").classList.remove("hidden");
 });
 
@@ -14,5 +16,6 @@ listClose.addEventListener("click", function () {
   menu.classList.remove("hidden");
   list.classList.add("hidden");
   listClose.classList.add("hidden");
+  nameShrank.classList.remove("shrank");
   document.querySelectoral(".list-item").classList.add("hidden");
 });
